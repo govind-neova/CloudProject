51136e5dc40c533ea03bad422855e21e87f78b7f
Govind Saraf @govind_saraf <govind_saraf@neovasolutions.com>
2019-12-20 17:36:12 +0530

    2.8 updated

--- 2.8_ensure_rotation_for_customer_created_CMKs_is_enabled.py
+++ 2.8_ensure_rotation_for_customer_created_CMKs_is_enabled.py
@@ -8,13 +8,6 @@ from os import path
 sys.path.insert(0, '../')
 from common import *
 
-
-#Declairing user input variable
-varUserInput = func_user_input_validation()
-
-#Declairing scriptname for json file
-varScriptName = sys.argv[0]
-
 #Global variables
 kms_client = boto3.client('kms')
 kms_response = kms_client.list_keys(
@@ -50,68 +43,64 @@ def func_kmsKeys_details (varDecision):
     return keys
             
 
-kmsKeys_compliant = func_kmsKeys_details (None)
+kmsKeys_compliant = func_kmsKeys_details (True)
 for i in kmsKeys_compliant:
     for j in kmsKeys_compliant[i]:
         print (j)
 
-#kmsKeys_nonCompliant = func_kmsKeys_details_nonCompliant ()
-#
-##Function to list customer managed keys kmsKeys
-#def func_kmsKeys_list_cmks ():
-#    for i in kms_response['Keys']:
-#        try:
-#            keyState = kms_client.describe_key(KeyId=i['KeyId'])
-#            if "PendingDeletion" not in str(keyState['KeyMetadata']['KeyState']) and "Default master key that protects my" not in str(keyState['KeyMetadata']['Description']):
-#                status = kms_client.describe_key(KeyId=i['KeyId'])
-##                yield (i['KeyId'])
-#                yield i
-#                print (status['KeyMetadata']['Enabled'])
-#        except Exception as e:
-#            print(e)
-#
-#kmsKeys_list_cmks = func_kmsKeys_list_cmks ()
-#
-##Function to create compliant key
-#def func_create_compliant_key ():
-#    kms_response = kms_client.create_key(Description="cis-testing-key")
-#    print (kms_response['KeyMetadata']['KeyId'])
-#    kms_client.enable_key_rotation(KeyId=kms_response['KeyMetadata']['KeyId'])
-#
-##Function to create non-compliant key
-#def func_create_non_compliant_key ():
-#    kms_response = kms_client.create_key(Description="cis-testing-key")
-#
-##If loop for performing operations on Cloudtrails as per config.yaml file
-#if varUserInput == 'nonCompliantUpdate':
-#    log.info('Non-compliant and Update')
-#    for value in func_kmsKeys_details_compliant ():
-#        print (value['KeyArn'])
-#        kms_client.disable_key_rotation(KeyId=value['KeyId'])
-#    func_create_non_compliant_key ()
-#elif varUserInput == 'compliantUpdate':
-#    log.info('compliant and Update')
-#    for value in func_kmsKeys_details_nonCompliant ():
-#        rotationStatus = kms_client.get_key_rotation_status(KeyId=value['KeyId'])
-#        status = kms_client.describe_key(KeyId=value['KeyId'])
-#        if (status['KeyMetadata']['Enabled']) is True :
-#            if rotationStatus['KeyRotationEnabled'] is False :
-#                print (value['KeyArn'])
-#                kms_client.enable_key_rotation(KeyId=value['KeyId'])
-#    func_create_compliant_key ()
-#elif varUserInput == 'compliantDelete':
-#    log.info('compliant and Delete')
-#    for value in func_kmsKeys_list_cmks () :
-#        print (value['KeyArn'])
-#        kms_client.schedule_key_deletion(KeyId=value['KeyId'])
-#    func_create_compliant_key ()
-#elif varUserInput == 'nonCompliantDelete':
-#    log.info('compliant and Delete')
-#    for value in func_kmsKeys_list_cmks () :
-#        print (value['KeyArn'])
-#        kms_client.schedule_key_deletion(KeyId=value['KeyId'])
-#    func_create_non_compliant_key ()
-#elif varUserInput == 'createcompliant':
-#    func_create_compliant_key ()
-#elif varUserInput == "createnoncompliant":
-#    func_create_non_compliant_key ()
+#Function to create compliant key and non complianr keys
+def func_create_compliant_or_non_compliant_key (varDecision):
+    if varDecision is True:
+        log.info('Creating compliant key')
+        kms_response = kms_client.create_key()
+        print (kms_response['KeyMetadata']['KeyId'])
+        kms_client.enable_key_rotation(KeyId=kms_response['KeyMetadata']['KeyId'])
+    elif varDecision is False:
+        log.info('Creating non-compliant key')
+        kms_response = kms_client.create_key(Description="cis-testing-key")
+        print (kms_response['KeyMetadata']['KeyId'])
+
+#If loop for performing operations on Cloudtrails as per config.yaml file
+if func_user_input_validation() == 'nonCompliantUpdate':
+    log.info('Updating resources as non-compliant as per request')
+    log.info("Gathering information for compliant resources")
+    kmsKeys_compliant = func_kmsKeys_details (True)
+    for i in kmsKeys_compliant:
+        for j in kmsKeys_compliant[i]:
+            print(j)
+            kms_client.disable_key_rotation(KeyId=j)
+
+elif func_user_input_validation() == 'compliantUpdate':
+    log.info('Updating resources as compliant as per request')
+    log.info("Gathering information for non-compliant resources")
+    kmsKeys_compliant = func_kmsKeys_details (False)
+    for i in kmsKeys_compliant:
+        for j in kmsKeys_compliant[i]:
+            rotationStatus = kms_client.get_key_rotation_status(KeyId=j)
+            status = kms_client.describe_key(KeyId=j)
+            if (status['KeyMetadata']['Enabled']) is True :
+                if rotationStatus['KeyRotationEnabled'] is False :
+                    print (j)
+                    kms_client.enable_key_rotation(KeyId=j)
+
+elif func_user_input_validation() == 'compliantDelete':
+    log.info('The user request is to delete existing resource/resources and create a compliant resource')
+    kmsKeys_compliant = func_kmsKeys_details (None)
+    for i in kmsKeys_compliant:
+        for j in kmsKeys_compliant[i]:
+            kms_client.schedule_key_deletion(KeyId=j)
+    func_create_compliant_or_non_compliant_key (True)
+
+elif varUserInput == 'nonCompliantDelete':
+    log.info('The user request is to delete existing resource/resources and create a non-compliant resource')
+    kmsKeys_compliant = func_kmsKeys_details (None)
+    for i in kmsKeys_compliant:
+        for j in kmsKeys_compliant[i]:
+            kms_client.schedule_key_deletion(KeyId=j)
+    func_create_compliant_or_non_compliant_key (False)
+
+elif varUserInput == 'createcompliant':
+    func_create_compliant_or_non_compliant_key (True)
+
+elif varUserInput == "createnoncompliant":
+    func_create_compliant_or_non_compliant_key (False)
